2fb9edc3312ac6ea54497a887482e9c98bfd6da2
Fasina Israel <Fashman28@gmail.com>
2021-10-08 15:42:56 +0100

    billingInfoUnderchapterNine

---
+++ src/ChapterNine/BillingInfo.java
@@ -0,0 +1,8 @@
+//package ChapterNine;
+//
+//public class BillingInfo{
+//    private int receiversPhoneNumber;
+//    private String receiversName;
+//    private String deliveryAddress;
+//    private CreditCardInformation creditCardInformation;
+//}
