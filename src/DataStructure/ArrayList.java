723edada5b7db2f62f1ac71cdbb01133e872c6c5
Fasina Israel <Fashman28@gmail.com>
2021-10-08 11:16:40 +0100

    ArrayList

---
+++ src/DataStructure/ArrayList.java
@@ -0,0 +1,82 @@
+package DataStructure;
+
+public class ArrayList implements List {
+    private int size;
+    private int element;
+    private int[] elements;
+
+    public ArrayList(){
+        elements = new int[6];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void add(int element) {
+        boolean isFull = getCapacity() == elements.length;
+        int[] newArray = new int[elements.length * 2];
+        for (int i = 0; i < elements.length; i++) {
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+        elements[size++] = element;
+        //size++;
+    }
+
+    public int getCapacity() {
+        return elements.length;
+    }
+
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(int element, int position) {
+        elements[position] = element;
+    }
+
+    @Override
+    public int get(int index) {
+        return elements[index];
+    }
+
+    @Override
+    public int getIndexOf(int element) {
+        for (int i = 0; i < elements.length; i++) {
+            if(elements[i] == element)
+                return i;
+        }
+        return 0;
+    }
+
+    @Override
+    public void remove(int element){
+        if(isEmpty()) throw new IllegalArgumentException("Arraylist is empty");
+        size--;
+        int[] newArray = new int[elements.length];
+        int number = getIndexOf(element)+1;
+        for (int i =0; i < number; i++){
+            if(elements[i] == element){
+                for(int j = i; j < size; j++){
+                    newArray[j] = elements[j+i];
+                }
+            }else{
+                newArray[i] = elements[i];
+            }
+        }
+        elements = newArray;
+    }
+
+
+}
+
+
+
+
