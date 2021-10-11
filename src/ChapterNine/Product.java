6bd135eec555aa3e242db210ac7abcc3e6c254d0
Fasina Israel <Fashman28@gmail.com>
2021-10-11 13:43:18 +0100

    product

---
+++ src/ChapterNine/Product.java
@@ -0,0 +1,10 @@
+package ChapterNine;
+
+public class Product extends Items{
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private  double price;
+    private  ProductCategory category;
+
+}
