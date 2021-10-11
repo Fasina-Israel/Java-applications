34c6eaae48c8fa24dcaeb466ea9bfff047cab191
Fasina Israel <Fashman28@gmail.com>
2021-10-11 13:47:11 +0100

    user

---
+++ src/ChapterNine/Users.java
@@ -0,0 +1,13 @@
+package ChapterNine;
+
+public class Users extends ElectronicStore{
+
+    private int age;
+    private String email;
+    private String address;
+    private String password;
+    private String phone;
+    private String name;
+
+
+}
