d82ef16e530a629e8f906aa1f19bff57e8e023d9
Fasina Israel <Fashman28@gmail.com>
2021-10-12 09:24:56 +0100

    musicClass

---
+++ src/Mp3Project/Music.java
@@ -0,0 +1,15 @@
+package Mp3Project;
+
+public class Music {
+
+    private String trackName;
+    private int trackNumber;
+    private int Length;
+
+    public void setLength(){
+
+    }
+    public int getLength(){
+        return Length;
+    }
+}
