84ab176b68606089314d2a5c9461860b0055fd98
Fasina Israel <Fashman28@gmail.com>
2021-10-12 09:27:12 +0100

    playListClass

---
+++ src/Mp3Project/PlayList.java
@@ -0,0 +1,15 @@
+package Mp3Project;
+
+import java.util.ArrayList;
+
+public class PlayList {
+    ArrayList<Music> music = new ArrayList<Music>();
+
+    public void addMusic(){
+
+
+    }
+    public void deleteMusic(){
+
+    }
+}
