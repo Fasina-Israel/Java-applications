3cedd1142f070a003fd102cadd9c7444112cc1bb
Fasina Israel <Fashman28@gmail.com>
2021-10-19 19:15:51 +0100

    boot

---
+++ src/NewPracticalPackage/Boot.java
@@ -0,0 +1,25 @@
+package NewPracticalPackage;
+
+public class Boot {
+    private String name;
+    private int number;
+    private String rug;
+
+    public Boot(String name, int number, String rug) {
+        this.name = name;
+        this.number = number;
+        this.rug = rug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getRug() {
+        return rug;
+    }
+}
