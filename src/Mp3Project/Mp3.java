033df186d4b8cf65e65911cc323df32c026e3fd0
Fasina Israel <Fashman28@gmail.com>
2021-10-20 08:43:47 +0100

    mp3-class

--- src/Mp3Project/Mp3.java
+++ src/Mp3Project/Mp3.java
@@ -4,22 +4,28 @@ public class Mp3 {
     private boolean power;
     private Volume volume;
     private PlayList playList;
-    private PlayMode playMode;
-//    private MusicState musicState = MusicState.STOP;
+    private MusicState musicState = MusicState.STOP;
+    private PlayMode playMode =PlayMode.LINEAR;
 
 
     public  void setPower(){
-       this.power = true;
+
+        this.power = true;
     }
     public  boolean getOn() {
+
         return power;
     }
 
     public void offPower(){
+
         this.power = false;
     }
     public void playMusic(){
 
+    }
+    public void playingMode(){
+        this.playMode = playMode;
     }
 }
 
