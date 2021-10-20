d93dcb92402ec5e1edc0e866b79912c4b81f725f
Fasina Israel <Fashman28@gmail.com>
2021-10-20 22:50:04 +0100

    list;

---
+++ src/FashDataStructure/List.java
@@ -0,0 +1,23 @@
+package FashDataStructure;
+
+public interface List {
+
+    boolean isEmpty();
+
+    void add(int value);
+
+    void remove(int element);
+
+    int size();
+
+    void add(int element, int position);
+
+    int get(int index);
+
+    int getIndexOf(int element);
+
+    int getCapacity();
+
+    void insert(int element, int index);
+
+}
