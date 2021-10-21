1b78a280843dde0df272af20e00a870030e9539b
Fasina Israel <Fashman28@gmail.com>
2021-10-21 22:44:57 +0100

    main

--- src/TtdProjects/Main.java
+++ src/TtdProjects/Main.java
@@ -1,4 +1,13 @@
 package TtdProjects;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scan = new Scanner(System.in);
+    HumanList human = new HumanList();
+
+    public static void main(String[] args) {
+
+    }
 }
