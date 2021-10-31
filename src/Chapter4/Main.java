638583523ad92cd34f8a171478615ce9c80a5b4c
Fasina Israel <Fashman28@gmail.com>
2021-10-31 17:29:18 +0100

    ...

---
+++ src/Chapter4/Main.java
@@ -0,0 +1,11 @@
+package Chapter4;
+
+public class Main {
+
+    Time1 time1 = new Time1();
+    Time1 time2 = new Time1(10);
+    Time1 time3 = new Time1(10,24);
+    Time1 time4 = new Time1(10,24,22);
+    Time1 time5 = new Time1(time4);
+
+}
